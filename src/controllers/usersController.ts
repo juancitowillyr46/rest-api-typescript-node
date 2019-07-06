3a4d62186d437c82048563548d144393404c5c24
Juan Rodas <juan.rodas.manez@gmail.com>
2019-07-06 15:27:18 -0500

    Aplicando modulo de inicio de sesion

--- src/controllers/usersController.ts
+++ src/controllers/usersController.ts
@@ -1,9 +1,13 @@
-import { Router, Request, Response } from "express";
+import { Request, Response } from "express";
 import UserModel, { User } from "../models/User";
 import Joi from "@hapi/joi";
+import bcrypt from "bcryptjs";
 
 class UsersController {
   public async postUsers(req: Request, res: Response) {
+    const { email, fullname, password } = req.body;
+
+    // Validation
     const schema = {
       fullname: Joi.string()
         .min(6)
@@ -18,11 +22,23 @@ class UsersController {
     };
 
     const { error } = Joi.validate(req.body, schema);
-
     if (error) return res.status(400).send(error.details[0].message);
 
-    const { email, fullname, password } = req.body;
-    const user: User = new UserModel({ email, fullname, password });
+    // Check Email
+    const emailExist = await UserModel.findOne({ email: email });
+    if (emailExist) return res.status(400).send("Email already exists");
+
+    // Encrypt Passwords
+    const salt = await bcrypt.genSalt(10);
+
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    // Create a new User
+    const user: User = new UserModel({
+      email,
+      fullname,
+      password: hashedPassword
+    });
 
     try {
       const userSave = await user.save();
@@ -30,6 +46,30 @@ class UsersController {
     } catch (error) {
       res.status(400).send(error);
     }
+  }
+
+  public async login(req: Request, res: Response) {
+    const schema = {
+      username: Joi.string()
+        .min(6)
+        .email()
+        .required(),
+      password: Joi.string()
+        .min(6)
+        .required()
+    };
+    const { error } = Joi.validate(req.body, schema);
+    const user = await UserModel.findOne({ email: req.body.username });
+
+    if (error) return res.status(400).send(error.details[0].message);
+
+    if (!user) return res.status(400).send("Email not already exist");
+
+    const validPass = await bcrypt.compare(req.body.password, user.password);
+
+    if (!validPass) return res.status(400).send("Invalid password");
+
+    return res.status(200).send(req.body);
   }
 
   public getUsers(req: Request, res: Response) {
--- src/routes/usersRoutes.ts
+++ src/routes/usersRoutes.ts
@@ -1,12 +1,12 @@
-import { Router } from 'express';
-import usersController from '../controllers/usersController';
+import { Router } from "express";
+import usersController from "../controllers/usersController";
 
 const router: Router = Router();
 
-
-router.post('/', usersController.postUsers);
-router.get('/', usersController.getUsers);
-router.delete('/', usersController.deleteUsers);
-router.put('/', usersController.putUsers);
+router.post("/register", usersController.postUsers);
+router.post("/login", usersController.login);
+router.get("/", usersController.getUsers);
+router.delete("/", usersController.deleteUsers);
+router.put("/", usersController.putUsers);
 
 export default router;
